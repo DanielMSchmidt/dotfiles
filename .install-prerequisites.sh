3a98a88f2e4eef02baaf1bc9c4eb0155fa00c61f
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-07-25 11:08:42 +0200

    adding accounts

--- .install-prerequisites.sh
+++ .install-prerequisites.sh
@@ -10,19 +10,30 @@ else
     Darwin)
         brew install --cask 1password
         brew install 1password-cli
-
-        # TODO: Possibly check for accounts before signing in
-        op account add "EI6GPO6VNJAVLGDDID3B75JI6E"
-        read -p "Is this a work computer? (y/Y)" -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            op account add "2HZZS3CSKVA7REGL25XWFDGOPE"
-        fi
         ;;
     *)
         echo "unsupported OS"
         exit 1
         ;;
     esac
+fi
 
+# Check if the account is signed in
+PERSONAL_ACCOUNT="EI6GPO6VNJAVLGDDID3B75JI6E"
+WORK_ACCOUNT="2HZZS3CSKVA7REGL25XWFDGOPE"
+
+if op vault list --account $PERSONAL_ACCOUNT >/dev/null 2>&1; then
+    echo "Already logged into personal account"
+else
+    op account add --account $PERSONAL_ACCOUNT
+fi
+
+read -p "Is this a work computer? (y/Y)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if op vault list --account $WORK_ACCOUNT >/dev/null 2>&1; then
+        echo "Already logged into work account"
+    else
+        op account add --account $WORK_ACCOUNT
+    fi
 fi
