c4a28e4e5e81018d6fc16ed29aa50b0ed4f3734b
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-09-10 18:34:16 +0200

    add alias

--- dot_config/fish/conf.d/gh.fish
+++ dot_config/fish/conf.d/gh.fish
@@ -8,3 +8,7 @@ alias prc="gh pr view --json url | jq -r '.url' | pbcopy"
 alias prs="gh pr list -A '@me' --web"
 # Open the compare view for the current branch
 alias compare="open https://github.com/(gh repo view --json='owner' --jq='.owner.login')/(gh repo view --json=name --jq='.name')/compare/(git rev-parse --abbrev-ref HEAD)"
+# Create PR with PR template and open it in browser
+function pr_create --wraps rm --description 'creates a PR with a template and opens it in the browser'
+    gh pr create --web -b="$(cat ./.github/pull_request_template.md || git log main..HEAD -q)" $argv
+end
