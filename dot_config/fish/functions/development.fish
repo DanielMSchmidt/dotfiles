f24a9a213a53c3d82dc17e5a3428f2fe4c8af6da
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-09-30 11:06:58 +0200

    add new helpers

--- dot_config/fish/functions/development.fish
+++ dot_config/fish/functions/development.fish
@@ -9,3 +9,11 @@ end
 function jiramd -d "Puts markdown link to jira in clipboard"
     echo "[$(git branch --show-current)](https://hashicorp.atlassian.net/browse/$(git branch --show-current))" | pbcopy
 end
+
+function goTestAll -d "Runs all tests in the current directory without caching"
+    go test -v -count=1 ./...
+end
+
+function goTestFails -d "Runs all tests and finds fails"
+    goTestAll | grep "FAIL"
+end
