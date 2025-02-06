75084065f1e53c8f8d78cb544053dc330f2797e9
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-02-06 10:33:45 +0100

    make terraform helpers more universal

--- dot_config/fish/conf.d/terraform.fish
+++ dot_config/fish/conf.d/terraform.fish
@@ -1,18 +1,23 @@
 ## Terraform
 
+# Set a default terraform path for my local terraform repo (so I can override it)
+set -x TERRAFORM_PATH_STD $HOME/work/hashicorp/terraform
+set -x TERRAFORM_PATH_PRIVATE $HOME/work/hashicorp/terraform-private
+set -x TERRAFORM_PATH $TERRAFORM_PATH_STD
+
 # Execute local terraform binary
-alias tfl="$HOME/work/hashicorp/terraform/bin/terraform"
+alias tfl="$TERRAFORM_PATH/bin/terraform"
 # Build terraform binary
-alias tfb="fish -c 'cd $HOME/work/hashicorp/terraform && mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
+alias tfb="fish -c 'cd $TERRAFORM_PATH && mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
 # Build terraform binary for linux
-alias tfbl="fish -c 'cd $HOME/work/hashicorp/terraform && GOOS=linux GOARCH=amd64 mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
+alias tfbl="fish -c 'cd $TERRAFORM_PATH && GOOS=linux GOARCH=amd64 mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
 # Watch terraform binary build
-alias tfw="fish -c 'cd $HOME/work/hashicorp/terraform && gow -v -c build -v -ldflags=\"-X 'main.experimentsAllowed=yes'\" -o bin/'"
+alias tfw="fish -c 'cd $TERRAFORM_PATH && gow -v -c build -v -ldflags=\"-X 'main.experimentsAllowed=yes'\" -o bin/'"
 
 ## Stacks
 
 # Set my tf binary to the local one
-set -x TFSTACKS_TERRAFORM_BINARY $HOME/work/hashicorp/terraform/bin/terraform
+set -x TFSTACKS_TERRAFORM_BINARY $TERRAFORM_PATH/bin/terraform
 
 # set debug level
 set -x TFSTACKS_LOG_LEVEL trace
@@ -23,4 +28,4 @@ alias scli="$HOME/work/hashicorp/terraform-stacks-cli/dist/tfstacks"
 alias sclib="fish -c 'cd $HOME/work/hashicorp/terraform-stacks-cli && make build'"
 
 # Debug
-alias dlvtfrpc="fish -c 'cd $HOME/work/hashicorp/terraform && dlv attach (ps | grep 'terraform rpcapi' | head -1 | awk '{ print $1 }')'"
+alias dlvtfrpc="fish -c 'cd $TERRAFORM_PATH && dlv attach (ps | grep 'terraform rpcapi' | head -1 | awk '{ print $1 }')'"
