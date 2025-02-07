a2c4e10e8eafbac037800c02560949a624aa717e
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-02-07 16:21:30 +0100

    make specific private commands

--- dot_config/fish/conf.d/terraform.fish
+++ dot_config/fish/conf.d/terraform.fish
@@ -1,10 +1,10 @@
 ## Terraform
 
-# Set a default terraform path for my local terraform repo (so I can override it)
-set -x TERRAFORM_PATH_STD $HOME/work/hashicorp/terraform
+# Set a default terraform path for my local terraform repo
 set -x TERRAFORM_PATH_PRIVATE $HOME/work/hashicorp/terraform-private
-set -x TERRAFORM_PATH $TERRAFORM_PATH_STD
+set -x TERRAFORM_PATH $HOME/work/hashicorp/terraform
 
+## Normal Builds
 # Execute local terraform binary
 alias tfl="$TERRAFORM_PATH/bin/terraform"
 # Build terraform binary
@@ -13,6 +13,16 @@ alias tfb="fish -c 'cd $TERRAFORM_PATH && mise x -- go build -ldflags=\"-X 'main
 alias tfbl="fish -c 'cd $TERRAFORM_PATH && GOOS=linux GOARCH=amd64 mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
 # Watch terraform binary build
 alias tfw="fish -c 'cd $TERRAFORM_PATH && gow -v -c build -v -ldflags=\"-X 'main.experimentsAllowed=yes'\" -o bin/'"
+
+## Private Builds
+# Execute local terraform binary
+alias tflp="$TERRAFORM_PATH_PRIVATE/bin/terraform"
+# Build terraform binary
+alias tfbp="fish -c 'cd $TERRAFORM_PATH_PRIVATE && mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
+# Build terraform binary for linux
+alias tfblp="fish -c 'cd $TERRAFORM_PATH_PRIVATE && GOOS=linux GOARCH=amd64 mise x -- go build -ldflags=\"-X 'main.experimentsAllowed=yes'\" -v -o bin/'"
+# Watch terraform binary build
+alias tfwp="fish -c 'cd $TERRAFORM_PATH_PRIVATE && gow -v -c build -v -ldflags=\"-X 'main.experimentsAllowed=yes'\" -o bin/'"
 
 ## Stacks
 
