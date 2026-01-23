3eb2584ddbe7b81e912b5a158a20699d7cbb2830
Daniel Schmidt <danielmschmidt92@gmail.com>
2026-01-23 16:20:36 +0100

    add prsmd helper

--- dot_config/fish/conf.d/gh.fish
+++ dot_config/fish/conf.d/gh.fish
@@ -6,6 +6,9 @@ alias pr="gh pr view --web"
 alias prc="gh pr view --json url | jq -r '.url' | pbcopy"
 # Open all of my PRs
 alias prs="gh pr list -A '@me' --web"
+# Markdown list of all my PRs
+alias prsmd="gh pr list --author=\"@me\" --json title,url,number --template '{{range .}}- [#{{.number}} {{.title}}]({{.url}}){{\"\n\"}}{{end}}'"
+
 # Open the compare view for the current branch
 alias compare="open https://github.com/(gh repo view --json='owner' --jq='.owner.login')/(gh repo view --json=name --jq='.name')/compare/(git rev-parse --abbrev-ref HEAD)"
 # Create PR with PR template and open it in browser
